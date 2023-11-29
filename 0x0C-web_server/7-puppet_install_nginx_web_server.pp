d386475be7ad8d57de280553f01a2c5bcac6a300
Code-Gale <codewhiz001@gmail.com>
2023-11-29 03:05:40 +0100

    puppeteer

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,51 @@
+# Install and Configure nginx: implement a direction and also create a customer 404 page
+exec { 'update':
+  command => '/usr/bin/apt-get update -y',
+}
+
+package { 'nginx':
+  ensure  => 'installed',
+}
+
+file { 'homepage':
+  ensure  => 'file',
+  path    => '/var/www/html/index.html',
+  content => 'Hello World!',
+}
+
+file { '404-page':
+  ensure  => 'file',
+  path    => '/var/www/html/404.html',
+  content => 'Ceci n\'est pas une page',
+}
+$new_str="	location = /redirect_me {
+			return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
+		}"
+
+file_line {'redirect':
+  ensure => present,
+  line   => $new_str,
+  match  => '^}$',
+  before => '^}$',
+  path   => '/etc/nginx/sites-available/default',
+}
+
+$err_str="	error_page 404 /404.html;
+
+		location = /404.html {
+			internal;
+		}"
+
+exec {'handle_404':
+  ensure => present,
+  line   => $err_str,
+  match  => '^}$',
+  before => '^}$',
+  path   => '/etc/nginx/sites-available/default',
+}
+
+service {'nginx':
+  ensure  => 'running',
+  enable  => true,
+  require => Package['nginx'],
+}
